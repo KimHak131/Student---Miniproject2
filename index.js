581ddd6058455721e9aebc343fbfcd3e81b5132f
Kimhak Oul <137867485+KimHak131@users.noreply.github.com>
2025-05-29 21:36:47 +0700

    update

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
-const e1 = require('express');
-const app = e1();
+const express = require('express');
+const app = express();
 
-//REG API
-app.post('/registration', (req, res) => {
-    res.send('<html><body>INSIDE REGISTRATION API..</body></html>');
+// SUBMIT ASSIGNMENT API
+app.post('/submitassignment', (req, res) => {
+    res.send(`
+        <html>
+            <head><title>Submit Assignment</title></head>
+            <body>
+                <h1>INSIDE SUBMIT ASSIGNMENT API</h1>
+            </body>
+        </html>
+    `);
 });
 
-app.post('/login', (req, res) => {
-    res.send('<html><body>INSIDE LOGIN API..</body></html>');
+// VIEW ASSIGNMENT API
+app.get('/viewassignment', (req, res) => {
+    res.send(`
+        <html>
+            <head><title>View Assignment</title></head>
+            <body>
+                <h1>INSIDE VIEW ASSIGNMENT API</h1>
+            </body>
+        </html>
+    `);
 });
 
-//SEARCH API
-app.get('/search', (req, res) => {
-    res.send('<html><body>INSIDE SEARCH API..</body></html>');
+// EDIT PROFILE API
+app.put('/editprofile', (req, res) => {
+    res.send(`
+        <html>
+            <head><title>Edit Profile</title></head>
+            <body>
+                <h1>INSIDE EDIT PROFILE API</h1>
+            </body>
+        </html>
+    `);
 });
 
-//VIEW API
-app.get('/view', (req, res) => {
-    res.send('<html><body>INSIDE VIEW API..</body></html>');
+// START THE SERVER ON PORT 5010
+app.listen(5010, () => {
+    console.log('EXPRESS Server Started at Port No: 5005');
 });
-
-//UPDATE API
-app.put('/updateprofile', (req, res) => {
-    res.send('<html><body>INSIDE UPDATE PROFILE API..</body></html>');
-});
-
-//DELETE API
-app.delete('/del', (req, res) => {
-    res.send('<html><body>INSIDE DELETE API..</body></html>');
-});
-
-// START THE EXPRESS SERVER. 5000 is the PORT NUMBER
-app.listen(5000, () =>
-    console.log('EXPRESS Server Started at Port No: 5000'));
-
-
